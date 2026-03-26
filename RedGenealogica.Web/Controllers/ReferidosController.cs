29b9ee025d0e44600ee3b686169ff3e31e2bf66f
Vasquez Franco <franco.vasquez9897@gmail.com>
2026-03-25 23:55:45 -0300

    Registrar referido

---
+++ RedGenealogica.Web/Controllers/ReferidosController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RedGenealogica.Web.Models;
+using RedGenealogica.Web.Services;
+using RedGenealogica.Web.ViewModels;
+
+namespace RedGenealogica.Web.Controllers;
+
+[Authorize]
+public class ReferidosController : Controller
+{
+    private readonly UserManager<Usuario> _userManager;
+    private readonly ServicioReferidos _servicioReferidos;
+
+    public ReferidosController(
+        UserManager<Usuario> userManager,
+        ServicioReferidos servicioReferidos)
+    {
+        _userManager = userManager;
+        _servicioReferidos = servicioReferidos;
+    }
+
+    [HttpGet]
+    public IActionResult Crear()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Crear(RegistrarReferidoViewModel modelo)
+    {
+        var usuario = await _userManager.GetUserAsync(User);
+
+        if (usuario == null)
+            return RedirectToAction("Login", "Autenticacion");
+
+        var referido = await _servicioReferidos.RegistrarReferidoAsync(usuario.Id, modelo);
+
+        if (referido == null)
+        {
+            ModelState.AddModelError("", "Debes estar activo para referir");
+            return View(modelo);
+        }
+
+        return RedirectToAction("Panel", "Usuario");
+    }
+}
