07bf5594fd7285508ce8f4dd8b0af969de817c5b
Vasquez Franco <franco.vasquez9897@gmail.com>
2026-03-26 21:05:46 -0300

    Creando Ranking vista y controlador

---
+++ RedGenealogica.Web/Controllers/RankingController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RedGenealogica.Web.Data;
+
+namespace RedGenealogica.Web.Controllers;
+
+public class RankingController : Controller
+{
+    private readonly ContextoAplicacion _contexto;
+
+    public RankingController(ContextoAplicacion contexto)
+    {
+        _contexto = contexto;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var ranking = await _contexto.Users
+            .OrderByDescending(x => x.PuntosAcumulados)
+            .Take(10)
+            .ToListAsync();
+
+        return View(ranking);
+    }
+}
