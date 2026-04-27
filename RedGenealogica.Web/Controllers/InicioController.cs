803a6b5fbc20c94a992b7691d800580770de3eb8
Vasquez Franco <franco.vasquez9897@gmail.com>
2026-04-27 19:04:42 -0300

    Política de privacidad y términos de servicio

--- RedGenealogica.Web/Controllers/InicioController.cs
+++ RedGenealogica.Web/Controllers/InicioController.cs
@@ -44,4 +44,11 @@ public class InicioController : Controller
 
         return View();
     }
+
+    [HttpGet]
+    public IActionResult Privacidad() => View();
+
+    [HttpGet]
+    public IActionResult Terminos() => View();
+
 }
